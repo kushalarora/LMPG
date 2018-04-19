Fixed tie weights. Also, now normalizing reward only in the case of more than one reward.

policies.py
@@ -14,7 +14,7 @@ class LanguageBasePolicy(nn.Module):
         self.decoder = nn.Linear(self.config.hidden_size, vocab_size)
 
         if self.config.tie_weights:
-            assert self.config.embedding_dim == self.config.hidden_size, \
+            assert self.config.embedding_size == self.config.hidden_size, \
                     "Hidden layer size ({}) != Embedding layer size ({})" \
                         .format(self.config.embedding_size, self.config.hidden_size)
             self.encoder.weight = self.decoder.weight

train/reinforce.py
@@ -27,7 +27,8 @@ class Reinforce(BasePolicyGradient):
             rewards.insert(0, R)
 
         rewards = torch.Tensor(rewards)
-        # rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
+        if len(rewards) > 1:
+            rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
         for log_prob, entropy, reward in zip(self.saved_log_probs, self.entropies, rewards):
             policy_loss += -log_prob * reward -  0.0001 * entropy
 